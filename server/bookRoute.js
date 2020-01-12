ccbcf151a274672c507b0a1d058df876bfb939c1
LolnationCH <lolnationCH@gmail.com>
2020-01-12 15:07:20 -0500

    simple server

---
+++ server/bookRoute.js
@@ -0,0 +1,30 @@
+var express = require('express'),
+    router = express.Router();
+
+const jsonfile = require('jsonfile');
+const file = 'data.json'
+
+router.get('/', (req, res) => {
+  jsonfile.readFile(file, function (err, bookArray) {
+    if (err) { 
+      console.error(err) 
+    }
+    else {
+      res.status(200).json(bookArray); 
+    }
+  })
+});
+
+router.post('/', (req, res) => {
+  jsonfile.writeFile(file, req.body)
+    .then(result => {
+      console.log('Written to Database');
+      res.status(200).send("Updated database");
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(404).send("fail to update the database");
+    });
+})
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,16 @@
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 6969;
+
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.listen(port);
+
+console.log('todo list RESTful API server started on: ' + port);
+
+var bookRoute = require('./bookRoute');
+
+// Import my test routes into the path '/test'
+app.use('/books', bookRoute);
